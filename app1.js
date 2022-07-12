ddab7904f9b1656285dc9592e44482bbfb32baa1
Bluerose101 <thanhhong10194@gmail.com>
2022-07-11 23:57:43 -0400

    finish #1"

--- app1.js
+++ app1.js
@@ -2,6 +2,8 @@ const container = document.querySelector('.container');
 const resizeButton = document.querySelector('.resize-button button');
 const resetButton= document.createElement('button');
 const BlackButton = document.createElement('button');
+const OrangeButton = document.createElement('button');
+const RGBButton = document.createElement('button');
 const colorButton_div = document.querySelector('.color-buttons');
 
 //create grids
@@ -39,17 +41,27 @@ resizeButton.addEventListener('click',() => {
         userSize = Number(prompt ("Ops! Something is wrong. Please make sure it's under 100"));
     }
     createGrid(userSize);
+    reset();
+    blackColor();
+    OrangeColor();
+    rgbColor();
 })
 
 // create reset button
+function reset () {
+
+const boxes = container.querySelectorAll('.grid-box');
 resetButton.textContent = "RESET";
 resetButton.addEventListener('click', () => {
-    reSet();
+    boxes.forEach(box => {
+        box.style.background= '#9DC6CA';
+    })
 });
 colorButton_div.appendChild(resetButton).classList.add('button');
+}
+reset();
 
-
-//create color button
+//create color buttons
 function blackColor() {
    
     const boxes = container.querySelectorAll('.grid-box')
@@ -61,4 +73,34 @@ function blackColor() {
     })
     colorButton_div.appendChild(BlackButton).classList.add('button')
 }
-blackColor() 
+blackColor() ;
+
+function OrangeColor() {
+   
+    const boxes = container.querySelectorAll('.grid-box')
+    OrangeButton.textContent = 'ORANGE';
+    OrangeButton.addEventListener('click', function () {
+        boxes.forEach(box => box.addEventListener('mouseenter', function() {
+            box.style.background = '#FB9F8B'
+        }))
+    })
+    colorButton_div.appendChild(OrangeButton).classList.add('button')
+}
+OrangeColor() ;
+
+function rgbColor() {
+    
+    const boxs = container.querySelectorAll('.grid-box')
+    RGBButton.textContent = "RGB"
+    RGBButton.addEventListener('click' ,() => {
+        boxs.forEach(box => box.addEventListener('mouseenter', () => {
+            let R = Math.floor(Math.random() * 256);
+            let G = Math.floor(Math.random() * 256);
+            let B = Math.floor(Math.random() * 256);
+            const RGB = `rgb(${R},${G},${B})`;
+            box.style.background = RGB;
+        }))
+    })
+    colorButton_div.appendChild(RGBButton).classList.add('button');
+}
+rgbColor();
